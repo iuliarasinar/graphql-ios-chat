302b4f987a7832a696417bc686e9a7a33ddb2a80
Iulia Rasinar <iulia.rasinar@gmail.com>
2018-07-17 22:38:04 +0200

    Initial Commit

---
+++ graphql-ios-chatTests/graphql_ios_chatTests.swift
@@ -0,0 +1,36 @@
+//
+//  graphql_ios_chatTests.swift
+//  graphql-ios-chatTests
+//
+//  Created by Iulia Rasinar on 17.07.18.
+//  Copyright © 2018 Iulia Rasinar. All rights reserved.
+//
+
+import XCTest
+@testable import graphql_ios_chat
+
+class graphql_ios_chatTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
